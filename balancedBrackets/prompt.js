4222753e922f268f92584163bd3f711ed1598517
Angel Alvarado <alvarado399061@gmail.com>
2019-12-17 10:00:40 -0600

    attempts to solve problem

--- balancedBrackets/prompt.js
+++ balancedBrackets/prompt.js
@@ -9,4 +9,36 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    //create results variable;
+    var results = [];
+    //create object to hold options
+    var options = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    //iterate through string;
+    for (let i = 0; i < str.length; i++){
+        //if bracket equals an opening bracket;
+        if (str[i] === '(' || str[i] === '[' || str[i] === '{'){
+            //push to results;
+            results.push(str[i]);
+        }
+        //else pop from results
+        else {
+            let closed = results.pop();
+            //if element doesn't equal obj[popped element]
+            if (str[i] !== options[closed]){
+            //return false
+            return false;
+            }
+        }
+    }
+    //if results length doesnt equal 0 return false
+    if (results.length !== 0){
+        return false;
+    
+    }
+        return true;
+}
