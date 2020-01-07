b3128342729c128b99601461363baa126f9a5c31
Angel Alvarado <alvarado399061@gmail.com>
2020-01-07 09:33:38 -0600

    implements translate roman numerals

--- romanNumeralTranslator/prompt.js
+++ romanNumeralTranslator/prompt.js
@@ -28,4 +28,30 @@ var DIGIT_VALUES = {
 
 var translateRomanNumeral = function(romanNumeral) {
   // TODO: Implement me!
+  // split the string at each character
+  let temp = romanNumeral.split("");
+  // create result variable
+  let result = 0;
+  // iterate through array
+  for (let i = 0; i < temp.length; i++) {
+    //save current letter
+    let currentLetter = DIGIT_VALUES[temp[i]];
+    // save next letter
+    let nextLetter = DIGIT_VALUES[temp[i + 1]];
+    // if the current letter is undefined return null
+    if (currentLetter === undefined) {
+      return null;
+    } else {
+      /// else if current letter is less than the next letter subtract
+      if (currentLetter < nextLetter) {
+        result += nextLetter - currentLetter;
+        i++;
+      } else {
+        // else add to result
+        result += currentLetter;
+      }
+    }
+  }
+  return result;
 };
+
