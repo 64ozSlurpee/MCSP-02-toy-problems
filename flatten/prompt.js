b8d621a6f1a37c12d105fda0ea7552499929ff77
Angel Alvarado <alvarado399061@gmail.com>
2020-01-11 18:42:26 -0600

    solves flatten toy problem

--- flatten/prompt.js
+++ flatten/prompt.js
@@ -3,4 +3,20 @@
 // Example:
 // flatten([1,['a', 2, [8, true], 7]]) returns [1, 'a', 2, 8, true, 7]
 
-function flatten(array) {}
+function flatten(arrays) {
+    //create results varisble
+    var result = [];
+    //iterate through arrays of the array
+    for (var i = 0; i < arrays.length; i++) {
+      //if element is not an array, push to results
+      if (!Array.isArray(arrays[i])) {
+        result.push(arrays[i]);
+      } else {
+        //if element is an array, re-assign result to current result concatenated with return of calling flatten on current element
+        result = result.concat(flatten(arrays[i]));
+      }
+    }
+    //return result array
+    return result;
+  }
+  
