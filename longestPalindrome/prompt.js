910ca9b4085b317ca1dca0415260f96703797ebc
Angel Alvarado <alvarado399061@gmail.com>
2020-01-17 09:40:21 -0600

    solves longestPalingdrome

--- longestPalindrome/prompt.js
+++ longestPalindrome/prompt.js
@@ -6,4 +6,41 @@
  * whitespace on each side of dad).
  */
 
-var longestPalindrome = function(string) {};
+ //write function to check if string is a palindrome
+function isPalindrome(string) {
+    //split string, reverse array then join again
+    let reversedStr = string.split("").reverse().join("");
+    //return true if the given string is the same as the reversed string
+    return string === reversedStr;
+  }
+    
+  function longestPalindrome(string){
+    //declare max palindrome length and results variables
+    let maxLength = 0;
+    let results = "";
+    //iterate throught the given string
+    for(var i = 0; i < string.length; i++) {
+      //declare a substring variable
+      var sub = string.substring(i, string.length); 
+      //iterate through substring
+      for(var j = sub.length; j >= 0; j--) {
+        //declare another substring variable of the substring
+        var subStr = sub.substring(0, j);
+        //set conditional checking if substrings length is less than or equal to one
+        if (subStr.length <= 1){
+              continue;
+        }
+        //set conditional invoking isPalindrome function
+        if (isPalindrome(subStr)) {
+          //set conditional to check lengths
+          if (subStr.length > maxLength) {
+            //reassign variables
+            maxLength = subStr.length;
+            results = subStr;
+          }
+        }
+      }
+    }
+    //return results
+    return results;
+  }
